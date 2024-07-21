2efbc042dd1b8cd7ae3b6ae7507a089d6cad973b
Websiteclayton <132654403+Websiteclayton@users.noreply.github.com>
2024-07-21 14:37:20 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function greet(name, lastName){
+    console.log('hello ' + name + ''+lastName);
+}
+
+function square(number){
+    return number * number;
+}
+console.log("hello world");
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+    print("hello world");
+  document.getElementById("demo").innerHTML = "Hello World";
+}
